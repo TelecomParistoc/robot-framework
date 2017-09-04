e4484ca5c1c88900aae5ae5da01ff08a5917907f
Alkanoor <juju.schou@gmail.com>
2017-09-04 21:39:07 +0200

    avec le squelette dans l'init et pas en dehors

--- framework/local_robot/big_robot.py
+++ framework/local_robot/big_robot.py
@@ -3,8 +3,6 @@ from utils import AX12, moving_interface  #This part is temporary : AX12 and Mov
 
 
 
-robot_skeleton = Robot()
-
 def open_AX12_pinces(r):
     r.AX12_pinces1.move_to((15, 300))
     r.AX12_pinces2.move_to((85, 70))
@@ -15,6 +13,8 @@ def useless_function(robot):
 
 
 def init():
+    robot_skeleton = Robot()
+    
     robot_skeleton.add_object(AX12(125), 'AX12_pinces1')
     robot_skeleton.add_object(AX12(132), 'AX12_pinces2')
     robot_skeleton.add_methods_of_object(moving_interface)
